8e840479543f3ebb1732b319cb529835bc197047
wjw78879 <wjw1190@126.com>
2019-11-15 22:55:48 +0800

    2019.11.15

--- MatricesHolder.h
+++ MatricesHolder.h
@@ -9,8 +9,16 @@ public:
     MatricesHolder();
     ~MatricesHolder();
 
+    Matrix& operator[] (int rhs) const;
 
+    void add();
+    void set(int number);
+
+    void print(int number);
+    void list();
 private:
+    vector<Matrix> m_matrices;
+    int m_numMatrices;
 };
 
 #endif
--- MatrixTest.cpp
+++ MatrixTest.cpp
@@ -7,16 +7,20 @@
 using namespace std;
 
 int main() {
-    double dat1[] = {1, 3, 1, 1, 2, 5, 2, 2, 1, 3, 8, 9, 1, 3, 2, 2};
-    Matrix mat1(dat1, 4, 4);
+    double dat1[] = {1, 3, 5, 2, 4, 6, 3, 5, 7, 4, 6, 8, 5, 7, 9};
+	double dat2[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10, 3, 5, 7, 9, 11};
+    Matrix mat1(dat1, 5, 3);
+	Matrix mat2(dat2, 3, 5);
     cout << "origin: " << endl;
     mat1.print();
-    cout << "determinant: " << mat1.det() << endl; 
+    //cout << "determinant: " << mat1.det() << endl; 
     cout << "transpose: " << endl;
     mat1.transpose().print();
-    cout << "adjoint: " << endl;
-    mat1.adjoint().print();
-    cout << "inverse: " << endl;
-    mat1.inverse().print();
+    //cout << "adjoint: " << endl;
+    //mat1.adjoint().print();
+    //cout << "inverse: " << endl;
+    //mat1.inverse().print();
+	cout << "1 * 2: " << endl;
+	(mat1 * mat2).print();
     return 0;
 }
