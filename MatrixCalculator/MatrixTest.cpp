7cf95f44c4135fccd1cb244a0bae2e78760f8d93
wjw78879 <wjw1190@126.com>
2019-11-21 22:05:36 +0800

    4000 test case

--- MatrixCalculator/MatrixTest.cpp
+++ MatrixCalculator/MatrixTest.cpp
@@ -7,6 +7,14 @@
 using namespace std;
 
 int main() {
+    Matrix n(4000, 4000);
+    for(int i = 0; i < 4000; i++) {
+        for(int j = 0; j < 4000; j++) {
+            n[i][j] = (i + j) % 30;
+        }
+    }
+    cout << "test" << endl;
+    n.reducedRowEchelonForm();
     MatricesHolder holder;
     holder.run();
     return 0;
